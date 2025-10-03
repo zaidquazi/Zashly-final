2e42daaab8f85a2d34203ee4133c159aed85647f
zaidquazi <zaidquazi412@email.com>
2025-10-03 11:40:37 +0530

    First commit

--- frontend/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
-});
--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "coffee",
-  setTheme: (theme) => {
-    localStorage.setItem("streamify-theme", theme);
-    set({ theme });
-  },
-}));
